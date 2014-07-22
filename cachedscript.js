610670cb09ab9c31a92b61298b28949bb2f733ad
everydaypanos <everydaypanos@gmail.com>
2014-07-22 18:48:42 +0300

    gg

--- cachedscript.js
+++ cachedscript.js
@@ -1,3 +1,9 @@
+/**
+	Usage
+	$.cachedScript( "ajax/test.js" ).done(function( script, textStatus ) {
+		console.log( textStatus );
+	});
+	*/
 (function($) {
   $.cachedScript = function(url, options) {
 		// Allow user to set any option except for dataType, cache, and url
